d136f2171ed6a3e3719a6278dc3f154cde4801df
Mercy Amondi <mercyamondi161@gmail.com>
2023-07-13 04:14:35 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,13 +4,14 @@
  * main - A program that prints a line using the puts  function
  * Return: Always 0 (Success)
  */
-int main(void)
- {
 
+int main(void)
+
+{
         puts("\"Programming is like building a multilingual puzzle");
         return (0);
 	
- }
+}
 
 
 
