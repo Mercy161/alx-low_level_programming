828346a3b4e4a98ff120f604fa017e8f36eb301e
Mercy Amondi <mercyamondi161@gmail.com>
2023-07-31 07:27:08 -0700

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,28 +1,25 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * *_strstr - locates a substring
- * @haystack: string to search in
- * @needle: substring to look for
- *
- * Return: pointer to the beginning of the located substring
- * or NULL if the substring is not found
+ * _strstr - Entry point
+ * @haystack: input
+ * @needle: input
+ * Return: Always 0 (Success)
  */
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j;
+        for (; *haystack != '\0'; haystack++)
+        {
+                char *l = haystack;
+                char *p = needle;
 
-	for (i = 0; haystack[i] != '\0'; i++)
-	{
-		for (j = 0; needle[j] != '\0'; j++)
-		{
-			if (haystack[i + j] != needle[j])
-				break;
-		}
-		if (!needle[j])
-			return (&haystack[i]);
-	}
-	return (NULL);
-}
- 24 changes: 24 additions & 0 deletions24  
+                while (*l == *p && *p != '\0')
+                {
+                        l++;
+                        p++;
+                }
+
+                if (*p == '\0')
+			return (haystack);
+        }
+        return (0);
+}  
